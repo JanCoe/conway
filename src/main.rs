53f255435c03bc4c0af69f27547bcbe01e6224a4
JanCoe <jan@jancoe.io>
2024-08-20 15:19:26 +0100

    Type alias for grid.

--- src/main.rs
+++ src/main.rs
@@ -9,36 +9,42 @@ use std::collections::HashMap;
 use std::io::stdout;
 use std::{thread, time};
 
-#[derive(Eq, Hash, PartialEq, Debug, Clone)]
+type Grid = HashMap<Point, usize>;
+
+#[derive(Eq, Hash, PartialEq, Debug, Clone, Default)]
 struct Point {
     row: usize,
     col: usize,
 }
 
-impl Point {
-    fn count_neighbours(&self, grid: &HashMap<Point, usize>) -> usize {
-        let rows = max(1, self.row) - 1..=self.row + 1;
-        let cols = max(1, self.col) - 1..=self.col + 1;
-        rows.cartesian_product(cols)
-            .map(|(row, col)| grid.get(&Point { row, col }).unwrap_or(&0))
-            .sum::<usize>()
-            - *grid.get(&self).unwrap()
-    }
+#[derive(Default)]
+struct Area {
+    grid: Grid,
+    dims: Point,
+}
 
-    fn next_state(&self, grid: &HashMap<Point, usize>) -> usize {
-        match self.count_neighbours(grid) {
-            2 => *grid.get(&self).unwrap(),
-            3 => 1,
-            _ => 0,
-        }
+fn count_neighbours(point: &Point, grid: &Grid) -> usize {
+    let rows = max(1, point.row) - 1..=point.row + 1;
+    let cols = max(1, point.col) - 1..=point.col + 1;
+    rows.cartesian_product(cols)
+        .map(|(row, col)| grid.get(&Point { row, col }).unwrap_or(&0))
+        .sum::<usize>()
+        - *grid.get(&point).unwrap()
+}
+
+fn next_state(point: &Point, grid: &Grid) -> usize {
+    match count_neighbours(&point, grid) {
+        2 => *grid.get(&point).unwrap(),
+        3 => 1,
+        _ => 0,
     }
 }
 
-fn print_grid(grid: &HashMap<Point, usize>, rows: usize, cols: usize) {
-    for row in 0..rows {
+fn print_grid(area: &Area) {
+    for row in 0..area.dims.row {
         println!();
-        for col in 0..cols {
-            match grid.get(&Point { row, col }) {
+        for col in 0..area.dims.col {
+            match area.grid.get(&Point { row, col }) {
                 Some(1) => print!("*"),
                 _ => print!("-"),
             };
@@ -46,32 +52,36 @@ fn print_grid(grid: &HashMap<Point, usize>, rows: usize, cols: usize) {
     }
 }
 
-fn update_grid(grid: &mut HashMap<Point, usize>, rows: usize, cols: usize) {
-    let old_grid = grid.clone();
+fn update_grid(area: &mut Area) {
+    let old_grid = area.grid.clone();
     for &ref point in old_grid.keys() {
-        grid.insert(point.clone(), point.next_state(&old_grid));
+        area.grid.insert(point.clone(), next_state(&point, &old_grid));
     }
     thread::sleep(time::Duration::from_millis(300));
-    print_grid(&grid, rows, cols);
+    print_grid(&area);
+}
+
+fn load_area() -> Area {
+    let data = include_str!("../data/grid.csv");
+    let mut area = Area::default();
+    for (row, line) in data.lines().enumerate() {
+        area.dims.row += 1;
+        for (col, state) in line.split(',').enumerate() {
+            area.grid.insert(Point { row, col }, state.parse().unwrap());
+        }
+    }
+    area.dims.col = data.lines().next().unwrap().split(',').count();
+    area
 }
 
 fn main() {
+    let mut area = load_area();
+
     let mut stdout = stdout();
     stdout.execute(Clear(ClearType::All)).unwrap();
 
-    let data = include_str!("../data/grid.csv");
-    let mut grid = HashMap::<Point, usize>::new();
-    let mut rows: usize = 0;
-    for (row, line) in data.lines().enumerate() {
-        rows += 1;
-        for (col, state) in line.split(',').enumerate() {
-            grid.insert(Point { row, col }, state.parse().unwrap());
-        }
-    }
-    let cols = data.lines().next().unwrap().split(',').count();
-
     for _ in 0..100 {
-        _ = stdout.execute(MoveUp(rows as u16));
-        update_grid(&mut grid, rows, cols);
+        _ = stdout.execute(MoveUp(area.dims.row as u16));
+        update_grid(&mut area);
     }
 }
